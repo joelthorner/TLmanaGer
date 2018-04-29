b658dc378a9eae0b7c197e3ddfa31dd2366b1e6b
Joel Torner <joeltorner@gmail.com>
2018-04-29 20:45:48 +0200

    Fixing bugs
    - Options save error
    - Refactoring
    - Clean files

--- js/options.js
+++ js/options.js
@@ -2,18 +2,6 @@ if (window.innerWidth > 500) {
 	$('body').addClass('tw')
 }
 
-// OPT BG
-chrome.storage.sync.get(['optBgLc'], function(result) {
-	if ($.type(result.optBgLc) == 'undefined') {
-		
-		chrome.storage.sync.set({optBgLc: $('#opt-bg-lc').val()}, function() {
-
-		});
-	}else{
-		$('#opt-bg-lc').val(result.optBgLc);
-	}
-});
-
 $('#opt-save').click(function(event) {
 	chrome.storage.sync.set({optBgLc: $('#opt-bg-lc').val()}, function() {
 		$('#opt-save-cont').addClass('shown');
@@ -21,21 +9,6 @@ $('#opt-save').click(function(event) {
 			$('#opt-save-cont').removeClass('shown');
 		}, 1700)
 	});
-});
-// end OPT BG
-
-
-// OPT photo
-chrome.storage.sync.get(['optProfilePhoto'], function(result) {
-	if ($.type(result.optProfilePhoto) == 'undefined') {
-		
-		chrome.storage.sync.set({optProfilePhoto: $('.opt-profile-set.selected img').attr('src') }, function() {
-
-		});
-	}else{
-		$('.opt-profile-set').removeClass('selected');
-		$('img[src="'+result.optProfilePhoto+'"]').parents('button').addClass('selected');
-	}
 });
 
 $('.opt-profile-set').click(function(event) {
@@ -45,4 +18,3 @@ $('.opt-profile-set').click(function(event) {
 		$self.addClass('selected');
 	});
 });
-// end OPT photo
