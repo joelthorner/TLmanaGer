aba774e3ecd0718d46e6eda21b4d53ecb4b02a3b
joelthorner <joeltorner@gmail.com>
2018-05-02 20:05:54 +0200

    Refactoring and add option #7

--- src/bg/background.js
+++ src/bg/background.js
@@ -19,7 +19,17 @@ chrome.runtime.onMessage.addListener(
             // execute the content script
             chrome.tabs.executeScript(null, { file: "/js/jquery-3.3.1.min.js" }, function() {
                 chrome.tabs.executeScript(null, { // defaults to the current tab
-                    file: "/js/contentscript.js", // script to inject into page and run in sandbox
+                    file: "/js/contentscriptAddGuides.js", // script to inject into page and run in sandbox
+                    allFrames: true // This injects script into iframes in the page and doesn't work before 4.0.266.0.
+                });
+            });
+            sendResponse({}); // sending back empty response to sender
+            break;
+        case "opt-test-tokenizer":
+            // execute the content script
+            chrome.tabs.executeScript(null, { file: "/js/jquery-3.3.1.min.js" }, function() {
+                chrome.tabs.executeScript(null, { // defaults to the current tab
+                    file: "/js/contentScriptTokenizer.js", // script to inject into page and run in sandbox
                     allFrames: true // This injects script into iframes in the page and doesn't work before 4.0.266.0.
                 });
             });
