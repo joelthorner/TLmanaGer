4b434d82e0dd23cfdf05b3fc0b78ec9362a5cb3a
joelthorner <joeltorner@gmail.com>
2018-05-04 19:44:28 +0200

    Follow works

--- js/mapping/contentScriptAddMappingBlck.js
+++ js/mapping/contentScriptAddMappingBlck.js
@@ -4,19 +4,23 @@ var _CSS = `
 			background-color: rgba(255, 0, 0, 0.15);
 			width: 200px;
 			height: 200px;
-			z-index: 2147483647;
+			z-index: 50000;
 			position: absolute;
 			border: 4px solid red;
 			border-radius: 4px;
 		}
-		.nwgrip, .negrip, .swgrip, .segrip, .ngrip, .egrip, .sgrip, .wgrip {
+		.tlg-block:hover .ui-resizable-handle,
+		.ui-resizable-resizing .ui-resizable-handle{
+			opacity: 1;
+		}
+		.ui-resizable-handle{
 			width: 10px;
 			height: 10px;
 			background-color: #ffffff;
 			border: 1px solid #000000;
 			position: absolute;
+			opacity: 0;
 		}
-
 		.negrip {
 			top:-6px; right:-6px;
 		}
@@ -53,6 +57,15 @@ var _CSS = `
 			z-index: 4;
 			position: absolute;
 		}
+		.tlg-block-text{
+			background-color: rgba(255, 0, 0, 0.15);
+			width: 100px;
+			height: 50px;
+			z-index: 51000;
+			position: absolute;
+			border: 4px solid red;
+			border-radius: 4px;
+		}
 	</style>
 `;
 
@@ -65,6 +78,7 @@ var _BLOCK = `
 	<div class="tlg-block-cont">
 		<!-- BLOCK COLOR -->
 		<div id="${thisId}" class="tlg-block" style="left: ${thisLeft}px;top:${thisTop}px">
+
 			<div class="ui-resizable-handle ui-resizable-nw nwgrip" id="${thisId}_nwgrip"></div>
 			<div class="ui-resizable-handle ui-resizable-ne negrip" id="${thisId}_negrip"></div>
 			<div class="ui-resizable-handle ui-resizable-sw swgrip" id="${thisId}_swgrip"></div>
@@ -73,7 +87,11 @@ var _BLOCK = `
 			<div class="ui-resizable-handle ui-resizable-e egrip" id="${thisId}_egrip"></div>
 			<div class="ui-resizable-handle ui-resizable-s sgrip" id="${thisId}_sgrip"></div>
 			<div class="ui-resizable-handle ui-resizable-w wgrip" id="${thisId}_wgrip"></div>
+
 			<div class="mover"></div>
+		</div>
+		<div id="${thisId}_TEXT" class="tlg-block-text" style="left: ${thisLeft + 250}px;top:${thisTop}px">
+			Lorem ipsum
 		</div>
 	</div>
 `;
@@ -85,9 +103,26 @@ console.log(_BLOCK);
 
 
 // APENDS
-/*if (!$('#CSS_TLG_CSS').length)*/ $('body').append(_CSS);
+if (!$('#CSS_TLG_CSS').length) $('body').append(_CSS);
 $('body').append(_BLOCK);
 
+// FUNCTIONS --------------------------------------
+function getTextBlockRelPos($block, $text){
+	var topBlock = parseInt($block.css('top')),
+		 leftBlock = parseInt($block.css('left')),
+		 topText = parseInt($text.css('top')),
+		 leftText = parseInt($text.css('left')),
+		 heightBlock = $block.outerHeight(true),
+		 widthBlock = $block.outerWidth(true);
+
+	var leftSpaceText = leftText - (leftBlock + widthBlock),
+		 topSpaceText = topText - (topBlock + heightBlock);
+
+	return {
+		leftSpaceText : leftSpaceText,
+		topSpaceText : topSpaceText
+	}
+}
 
 // EVENTS ------------------------------------------
 $('.tlg-block-cont').each(function(index, el) {
@@ -96,15 +131,47 @@ $('.tlg-block-cont').each(function(index, el) {
 		var tlgBlockId = '#' + $(this).find('.tlg-block').attr('id');
 		
 		// move drag
-		// $(tlgBlockId).draggabilly({
-		// 	containment: 'body',
-		// 	handle: '.mover'
-		// });
-		$(function(){
-			$(tlgBlockId).draggable({
-				handle: '.mover'
+		$(tlgBlockId)
+			.draggable({
+				handle: '.mover',
+				drag: function( event, ui ) {
+					// console.log(ui);
+					var $text = $(tlgBlockId + '_TEXT');
+					var $block = $(tlgBlockId),
+
+					topBlock = parseInt($block.css('top')),
+					leftBlock = parseInt($block.css('left')),
+					heightBlock = $block.outerHeight(true),
+					widthBlock = $block.outerWidth(true);
+
+					$text.css({
+						left: leftBlock + widthBlock + $text.data('x-rel').leftSpaceText,
+						top: topBlock + heightBlock + $text.data('x-rel').topSpaceText
+					});
+				}
 			});
+
+		$(tlgBlockId + '_TEXT')
+			.draggable({
+				drag: function( event, ui ) {
+
+				}
+			});
+
+		// guardar rel
+		var $text = $(tlgBlockId + '_TEXT');
+		var $block = $(tlgBlockId);
+		var data = getTextBlockRelPos($block, $text);
+
+		$text.data('x-rel', {
+			leftSpaceText: data.leftSpaceText,
+			topSpaceText: data.topSpaceText
 		});
+
+		// drag mantenir distance
+
+		// console.log(leftBlock,widthBlock,leftSpaceText);
+		// console.log($(tlgBlockId + '_TEXT').data('rel-distance'));
 
 		// resizers
 		$(tlgBlockId).resizable({
