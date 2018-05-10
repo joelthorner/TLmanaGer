d5aff2560d8a38e6d3a3ba12a1a4b1922ed4cb88
joelthorner <joeltorner@gmail.com>
2018-05-10 17:30:05 +0200

    Fix buf #20

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -44,6 +44,7 @@ chrome.extension.sendMessage({}, function(response) {
 								$('[ls="opensaas.utilUpdateCacheCategories"]').val('CAT UPDATE');
 								$('[ls="opensaas.utilReloadApps"]').val('APP UPDATE');
 								$('[ls="opensaas.utilReloadEvents"]').val('EVENT UPDATE');
+								$('[ls="opensaas.utilDownloadProjectCode"]').val('');
 
 								var style = `
 									<style>
@@ -103,7 +104,11 @@ chrome.extension.sendMessage({}, function(response) {
 											left: auto;
 											right: 3px;
 											width: 34px;
-											padding-top: 3px
+											padding-top: 3px;
+											background-image : url('data:image/svg+xml;utf8,<svg aria-hidden="true" fill="#FFF" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"></path></svg>');
+											background-size: 20px;
+											background-position: center;
+											background-repeat: no-repeat;
 										}
 										.opensaasWindow div.windowContent {
 											height: 0
@@ -135,11 +140,9 @@ chrome.extension.sendMessage({}, function(response) {
 								$('#windowContainer').after(
 									$(".opensaasWindow")
 								);
-								$(".opensaasWindow.window input.green.__downloadProjectCode__").value="↓";
 								$('.taskMenuLink').first().remove();
-								
 							}
-						}, 1000);
+						}, 975);
 					}
 
 				});
