dbbb9ed4e7a58a6e50d401954ea3cbc9a9f2ca3e
joelthorner <joeltorner@gmail.com>
2018-09-02 03:48:20 +0200

    Refactor options data

---
+++ js/optionsData.js
@@ -0,0 +1,226 @@
+var AVATARS /* The legend of Aang */ = [
+	{
+		img: "img/logo.svg",
+		title: "Pastilla"
+	}, {
+		img: "img/avatar/2b.jpg",
+		title: "2b - NieR Automata"
+	}, {
+		img: "img/avatar/bananas.jpg",
+		title: "Si te gusta el fruto del amor"
+	}, {
+		img: "img/avatar/bookerDeWitt.jpg",
+		title: "Booker DeWitt - Bioshock Infinite"
+	}, {
+		img: "img/avatar/deadpool.jpg",
+		title: "Deadpool"
+	}, {
+		img: "img/avatar/eleven.jpg",
+		title: "11 - Stranger Things"
+	}, {
+		img: "img/avatar/elizabeth.jpg",
+		title: "Elizabeth - Bioshock Infinite"
+	}, {
+		img: "img/avatar/homerSimpson.jpg",
+		title: "Homer Simpson"
+	}, {
+		img: "img/avatar/isaac.jpg",
+		title: "Isaac"
+	}, {
+		img: "img/avatar/kero.jpg",
+		title: "Kero - Sakura card captor"
+	}, {
+		img: "img/avatar/miaWallace.jpg",
+		title: "Mia Wallace - Pulp fiction"
+	}, {
+		img: "img/avatar/momo.jpg",
+		title: "Momo - The legend of Aang"
+	}, {
+		img: "img/avatar/nedFlanders.jpg",
+		title: "Ned Flanders"
+	}, {
+		img: "img/avatar/ori.jpg",
+		title: "Ori"
+	}, {
+		img: "img/avatar/phasma.jpg",
+		title: "Phasma - Star Wars"
+	}, {
+		img: "img/avatar/rey.jpg",
+		title: "Rey - Star Wars"
+	}, {
+		img: "img/avatar/tothless.jpg",
+		title: "Tothless - Como entrenar a tu dragon"
+	}, {
+		img: "img/avatar/vulpex.jpg",
+		title: "Vulpex - Star Wars"
+	}, {
+		img: "img/avatar/chloe.jpg",
+		title: "Chloe - Life is Strange"
+	}, {
+		img: "img/avatar/minecraft.jpg",
+		title: "Minecraft"
+	}, {
+		img: "img/avatar/grumpy.jpg",
+		title: "Grumpy meme"
+	}, {
+		img: "img/avatar/fsSociety.jpg",
+		title: "fsSociety - Mr Robot"
+	}, {
+		img: "img/avatar/songoku.jpg",
+		title: "Songoku - Bola de drac"
+	}, {
+		img: "img/avatar/chuchu.jpg",
+		title: "Chuchu - Utena"
+	}, {
+		img: "img/avatar/sailorMoon.jpg",
+		title: "Sailormoon"
+	}, {
+		img: "img/avatar/picachu.jpg",
+		title: "Picachu"
+	}, {
+		img: "img/avatar/ranma.jpg",
+		title: "Ranma"
+	}, {
+		img: "img/avatar/cawboyBeBop.jpg",
+		title: "Cawboy BeBop"
+	}, {
+		img: "img/avatar/doge.jpg",
+		title: "Doge meme"
+	}
+];
+
+var CHANGELOG = [
+
+	{ 
+		version: "2.0.4",
+		date: "2 Sep 2018",
+		lines: [
+			"Visual improvements",
+			"Contents improvements"
+		]
+	}, { 
+		version: "2.0.3",
+		date: "28 Ago 2018",
+		lines: [
+			"Fix wrong urls",
+			"Fix undefined options (v2.x)",
+			"Visual fixes",
+			"Add \"LC big controls\" background option",
+			"Add more avatars for the loles"
+		]
+	}, { 
+		version: "2.0.2",
+		date: "26 Ago 2018",
+		lines: [
+			"_locale language files"
+		]
+	}, { 
+		version: "2.0.1",
+		date: "26 Ago 2018",
+		lines: [
+			"Add default_locale to manifest"
+		]
+	}, { 
+		version: "2.0.0",
+		date: "26 Ago 2018",
+		lines: [
+			"New design",
+			"Popup actions improvements",
+			"Injection scripts improvements",
+			"Add options layout",
+			"New option background on/off",
+			"Add changelog",
+			"Font Awesome moved to default option",
+			"Guide lines load in session",
+			"New action show Template 2018 modules",
+			"Add confirm account testing action",
+			"New avatars",
+			"Add notify action"
+		]
+	}, { 
+		version: "1.0.20",
+		date: "23 Ago 2018",
+		lines: [
+			"Fix restricted access to Emilio Generator"
+		]
+	}, { 
+		version: "1.0.19",
+		date: "23 Ago 2018",
+		lines: [
+			"New restricted access to Emilio Generator"
+		]
+	}, { 
+		version: "1.0.18",
+		date: "15 Jun 2018",
+		lines: [
+			"Add emailGenerator access"
+		]
+	}, { 
+		version: "1.0.17",
+		date: "14 Jun 2018",
+		lines: [
+			"Add mapping beta"
+		]
+	}, { 
+		version: "1.0.16",
+		date: "1 Jun 2018",
+		lines: [
+			"Update icons"
+		]
+	}, { 
+		version: "1.0.15",
+		date: "24 May 2018",
+		lines: [
+			"Added feature pages os <a href=\"https://github.com/joelthorner/TLmanaGer/issues/21\" target=\"_blank\">#21</a>"
+		]
+	}, { 
+		version: "1.0.14",
+		date: "16 May 2018",
+		lines: [
+			"Forceview focus on correct host"
+		]
+	}, { 
+		version: "1.0.13",
+		date: "11 May 2018",
+		lines: [
+			"Bug fix <a href=\"https://github.com/joelthorner/TLmanaGer/issues/25\" target=\"_blank\">#25</a>",
+			"Fix init undefined options value",
+			"Remove console log",
+			"Add img refresh by get param <a href=\"https://github.com/joelthorner/TLmanaGer/issues/26\" target=\"_blank\">#26</a>"
+		]
+	}, { 
+		version: "1.0.12",
+		date: "10 May 2018",
+		lines: [
+			"Fix bug <a href=\"https://github.com/joelthorner/TLmanaGer/issues/16\" target=\"_blank\">#16</a>",
+			"Fix bug <a href=\"https://github.com/joelthorner/TLmanaGer/issues/18\" target=\"_blank\">#18</a>",
+			"Fix bug <a href=\"https://github.com/joelthorner/TLmanaGer/issues/19\" target=\"_blank\">#19</a>",
+			"Fix bug <a href=\"https://github.com/joelthorner/TLmanaGer/issues/20\" target=\"_blank\">#20</a>",
+			"Added new feature <a href=\"https://github.com/joelthorner/TLmanaGer/issues/23\" target=\"_blank\">#23</a> FA css bg",
+			"Added new feature <a href=\"https://github.com/joelthorner/TLmanaGer/issues/22\" target=\"_blank\">#22</a> Forceview cookie"
+		]
+	}, { 
+		version: "1.0.11",
+		date: "8 May 2018",
+		lines: [
+			"Fix bug <a href=\"https://github.com/joelthorner/TLmanaGer/issues/16\" target=\"_blank\">#16</a>",
+			"Fix bug, body js add class"
+		]
+	}, { 
+		version: "1.0.10",
+		date: "7 May 2018",
+		lines: [
+			"Hidden dev feature",
+			"Add Lc Os option mode",
+			"Bg option now for all LCs",
+			"New script inject"
+		]
+	}, { 
+		version: "1.0.8",
+		date: "4 May 2018",
+		lines: [
+			"Beta new feature (stable)"
+		]
+	}
+
+]
--- js/optionsVisual.js
+++ js/optionsVisual.js
@@ -23,4 +23,37 @@ $(document).ready(function() {
 		$('.panel').removeClass('active');
 		$('.' + $(this).data('panel')).addClass('active');
 	});
+
+	// Set avatars
+	var $gridAvatars = $('.grid-avatar');
+	$.each(AVATARS, function(index, avatar) {
+		$gridAvatars.append(`
+			<div class="avatar">
+				<input type="radio" id="radio-${index}" name="opt-profile-avatar" value="${avatar.img}">
+				<label for="radio-${index}">
+					<img src="../../${avatar.img}" title="${avatar.title}">
+				</label>
+			</div>
+		`);
+	});
+
+	// Set changelog
+	var $changelogList = $('.changelog-list');
+	$.each(CHANGELOG, function(index, item) {
+		$changelogList.append(`
+			<li class="mdc-list-item mdc-ripple-upgraded" data-mdc-auto-init="MDCRipple">
+				<svg class="mdc-list-item__graphic icon"><use xlink:href="#icon-merge"></use></svg>
+				<span class="mdc-list-item__text">
+					<span class="mdc-list-item__primary-text">v${item.version}</span>
+					<span class="mdc-list-item__secondary-text">${item.date}</span>
+				</span>
+			</li>
+			<li class="sublist">
+				<ul>
+					<li>${item.lines.join('</li><li>')}</li>
+				</ul>
+			</li>
+			<li class="mdc-list-divider" role="separator"></li>
+		`);
+	});
 });
