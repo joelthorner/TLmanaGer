aaeaff85c4090a3c880c9022bbc53c28480f37e2
joelthorner <joeltorner@gmail.com>
2018-09-21 17:15:12 +0200

    Improvements in the forceview

--- js/inject/force-forceview.js
+++ js/inject/force-forceview.js
@@ -3,9 +3,15 @@ chrome.storage.sync.get(['optDevForceview'], function(result) {
 	if (typeof result.optDevForceview == 'undefined') result.optDevForceview = true;
 
 	if (result.optDevForceview) {
-		if (location.href.match(/https*:\/\/[0-9]{1,5}(\.logicommerce\.net|\.igd\.production)/)) {
-			if (!document.cookie.includes('FORCEVIEW=1')) {
-				location = location.href + '?forceview=1'
+		if (location.href.match(/https?:\/\/(www)?\.?[0-9]{2,5}(\.logicommerce\.net|\.igd\.production)\/?/)) {
+			if (!document.cookie.toUpperCase().includes('FORCEVIEW=1')) {
+				if (!location.href.toUpperCase().includes('FORCEVIEW')) {
+					if (location.href.includes('?')) {
+						window.location = window.location.href + '&forceview=1'
+					} else {
+						window.location = window.location.href + '?forceview=1'
+					}
+				}
 			}
 		}
 	}
