e3a261ae9dbfcb29f160ad47ad5b26982a6f352e
joelthorner <joeltorner@gmail.com>
2018-10-31 21:14:53 +0100

    Toggle Secret Hack Fix

--- js/actions/secret-hack.js
+++ js/actions/secret-hack.js
@@ -3,17 +3,21 @@ $(document).ready(function() {
 	var font2 = chrome.extension.getURL('fonts/Deus-Antics.woff');
 	var font3 = chrome.extension.getURL('fonts/Deus-Antics.ttf');
 
-	$('head').append(`
-		<style>
-			@font-face {
-				font-family: 'Deus Antics';
-				src: url('${font1}') format('woff2'), url('${font2}') format('woff'), url('${font3}') format('truetype');
-				font-weight: normal;
-				font-style: normal;
-			}
-			html > body * , html > body * *, html > body * * *{
-				font-family: "Deus Antics" !important;
-			}
-		</style>
-	`)
+	if ($('#deus-antics-font').length) {
+		$('#deus-antics-font').remove();
+	} else {
+		$('head').append(`
+			<style id="deus-antics-font">
+				@font-face {
+					font-family: 'Deus Antics';
+					src: url('${font1}') format('woff2'), url('${font2}') format('woff'), url('${font3}') format('truetype');
+					font-weight: normal;
+					font-style: normal;
+				}
+				html > body * , html > body * *, html > body * * *{
+					font-family: "Deus Antics" !important;
+				}
+			</style>
+		`);
+	}
 });
