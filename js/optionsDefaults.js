ac71412424a1e1aef966846a7f11383debb90940
joelthorner <joeltorner@gmail.com>
2019-01-18 22:42:03 +0100

    Hot fix - add optionDefaults

---
+++ js/optionsDefaults.js
@@ -0,0 +1,20 @@
+var defaults = {	
+	optLcBgActive: true,	
+	optLcBgValue: {	
+		image: chrome.extension.getURL('img/background-default.jpg'),	
+		thumb: chrome.extension.getURL('img/background-default.jpg'),	
+		userName: 'Matteo Fusco',	
+		userLink: 'https://unsplash.com/@matteofusco?utm_source=TLmanaGer&utm_medium=referral',	
+		downloadLocation: ''	
+	},	
+	optLcPagridActive: false,	
+	optLcDevBarActive: true,	
+	optDevForceview: true,	
+	optDevStealFa : true,	
+	optDevFlushCfm : true,	
+	optProfileEmail: '',	
+	optProfilePass: '',	
+	optProfileAvatar: 'img/logo.svg',	
+	optLcBigControls: false,	
+	optLcHolidays: true	
+};
