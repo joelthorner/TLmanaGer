34c28690e991698baedd8bdc9e4c00ff34dc73a3
joelthorner <joeltorner@gmail.com>
2019-04-12 20:55:18 +0200

    Refactor extra-commands

--- js/inject/zendesk/extra-commands.js
+++ js/inject/zendesk/extra-commands.js
@@ -1,110 +1,113 @@
-var log = chrome.i18n.getMessage('commandExecuted_message');
+ExtraCommands = {
 
-function openPanelCustomerContext() {
-	var $panel = $('.ember-view.ticket-sidebar:visible');
-	$panel.find('[data-test-id="customer-context-tab-ticket"]').click();
+	timeout : null,
+	interval: null,
+	$selectedTab: undefined,
 
-	return $panel;
-}
+	init: function () {
+		this.logMessage = chrome.i18n.getMessage('commandExecuted_message');
+		this.run();
+	},
 
-function panelCustomerContextToggleCheck(id, $panel) {
-	setTimeout(() => {
-		var $check = $panel.find('.custom_field_' + id + ' .ember-checkbox');
-		if ($check.length) {
-			$panel.find('.sidebar_box_container').scrollTop($check.position().top);
-			$check.click();
-		}
-	}, 100);
-}
+	run: function() {
+		this.setTicketKeyCommands();
+		this.setTabNavKeyCommands();
+		this.documentEvents();
+	},
 
-hotkeys('ctrl+shift+alt+a,command+shift+alt+a', function (event, handler) {
-	console.log(log.replace('%command%', 'zendesk-assignee-focus ' + handler.key));
+	documentEvents: function () {
+		$(document).on('click.ExtraCommandsTabLi', '#tabs li', function (event) {
+			ExtraCommands.$selectedTab = $(this);
+		});
 
-	var $paneTabTicket = openPanelCustomerContext();
-	setTimeout(() => {
-		$paneTabTicket
-			.find('.assignee_id .zd-selectmenu-base, .assignee_id .zd-searchmenu-base')
-			.focus();
-	}, 100);
-});
+		$(document).on('click.ExtraCommandsClearFocus', function (event) {
+			ExtraCommands.clearAllTimings();
+		});
+	},
 
-hotkeys('ctrl+shift+alt+t,command+shift+alt+t', function (event, handler) {
-	console.log(log.replace('%command%', 'zendesk-ticket-type-focus ' + handler.key));
+	clearAllTimings: function () {
+		clearTimeout(ExtraCommands.timeout);
+		clearInterval(ExtraCommands.interval);
+	},
 
-	var $paneTabTicket = openPanelCustomerContext();
-	setTimeout(() => {
-		$paneTabTicket
-			.find('.ticket_type_id .zd-selectmenu-base, .ticket_type_id .zd-selectmenu-focus-proxy')
-			.focus();
-	}, 100);
-});
-
-hotkeys('ctrl+shift+alt+p,command+shift+alt+p', function (event, handler) {
-	console.log(log.replace('%command%', 'zendesk-ticket-priority-focus ' + handler.key));
-
-	var $paneTabTicket = openPanelCustomerContext();
-	setTimeout(() => {
-		$paneTabTicket
-			.find('.priority_id .zd-selectmenu-base, .priority_id .zd-selectmenu-focus-proxy')
-			.focus();
-	}, 100);
-});
-
-hotkeys('ctrl+shift+alt+c,command+shift+alt+c', function (event, handler) {
-	console.log(log.replace('%command%', 'zendesk-toggle-check-comment-cc-team ' + handler.key));
-
-	var $paneTabTicket = openPanelCustomerContext();
-	panelCustomerContextToggleCheck(360016944932, $paneTabTicket);
-});
-
-hotkeys('ctrl+shift+alt+d,command+shift+alt+d', function (event, handler) {
-	console.log(log.replace('%command%', 'zendesk-toggle-check-doubts ' + handler.key));
-	
-	var $paneTabTicket = openPanelCustomerContext();
-	panelCustomerContextToggleCheck(360016976631, $paneTabTicket);
-});
-
-// tabs commands
-var $selectedTab, blur_i, blur_o;
-var observer = new MutationObserver(function (mutations) {
-	mutations.forEach(function (mutation) {
+	onFindTabSelected: function (mutation) {
 		if (mutation.addedNodes.length) {
-			var $findedTabSelected = $(mutation.target).find('.tab.selected');
-			if ($findedTabSelected.length) {
-				$selectedTab = $findedTabSelected;
-			}
+			var $selectedTab = $(mutation.target).find('.tab.selected');
+			if ($selectedTab.length) ExtraCommands.$selectedTab = $selectedTab;
 		}
-	});
-});
+	},
 
-observer.observe($('#tabs')[0], {
-	attributes: false,
-	childList: true,
-	characterData: false,
-	subtree: true
-});
+	setTicketKeyCommands: function () {
+		hotkeys('ctrl+shift+alt+a,command+shift+alt+a', function (event, handler) {
+			console.log(ExtraCommands.logMessage.replace('%command%', 'zendesk-assignee-focus ' + handler.key));
+			ExtraCommands.focusOnPanelCustomer({
+				'.assignee_id .zd-selectmenu-base': { event: 'focus', scrollTo: false },
+				'.assignee_id .zd-searchmenu-base': { event: 'focus', scrollTo: false }
+			});
+		});
 
-$(document).on('click', '#tabs li', function(event) {
-	$selectedTab = $(this);
-});
-$(document).on('click', function (event) {
-	clearTimeout(blur_o);
-	clearInterval(blur_i);
-});
+		hotkeys('ctrl+shift+alt+t,command+shift+alt+t', function (event, handler) {
+			console.log(ExtraCommands.logMessage.replace('%command%', 'zendesk-ticket-type-focus ' + handler.key));
+			ExtraCommands.focusOnPanelCustomer({
+				'.ticket_type_id .zd-selectmenu-base': { event: 'focus', scrollTo: false },
+				'.ticket_type_id .zd-selectmenu-focus-proxy': { event: 'focus', scrollTo: false }
+			});
+		});
 
-hotkeys('ctrl+alt+9,command+alt+9,ctrl+alt+0,command+alt+0', function (event, handler) {
-	var type = 'next';
-	if (handler.key == 'ctrl+alt+9' || handler.key == 'command+alt+9') type = 'prev';
-	
-	console.log(log.replace('%command%', 'zendesk-tab-' + type + ' ' + handler.key));
+		hotkeys('ctrl+shift+alt+p,command+shift+alt+p', function (event, handler) {
+			console.log(ExtraCommands.logMessage.replace('%command%', 'zendesk-ticket-priority-focus ' + handler.key));
+			ExtraCommands.focusOnPanelCustomer({
+				'.priority_id .zd-selectmenu-base': { event: 'focus', scrollTo: false },
+				'.priority_id .zd-selectmenu-focus-proxy': { event: 'focus', scrollTo: false }
+			});
+		});
 
-	var $temp = $selectedTab[type]();
-	if ($temp.length && !$temp.is('.add')) {
-		$temp.click();
-		
-		clearTimeout(blur_o);
-		clearInterval(blur_i);
-		blur_i = setInterval(() => { $('*').blur(); }, 1);
-		blur_o = setTimeout(() => { clearInterval(blur_i); }, 2000);
+		hotkeys('ctrl+shift+alt+c,command+shift+alt+c', function (event, handler) {
+			console.log(ExtraCommands.logMessage.replace('%command%', 'zendesk-toggle-check-comment-cc-team ' + handler.key));
+			ExtraCommands.focusOnPanelCustomer({
+				'.custom_field_360016944932 .ember-checkbox': { event: 'click', scrollTo: true }
+			});
+		});
+
+		hotkeys('ctrl+shift+alt+d,command+shift+alt+d', function (event, handler) {
+			console.log(ExtraCommands.logMessage.replace('%command%', 'zendesk-toggle-check-doubts ' + handler.key));
+			ExtraCommands.focusOnPanelCustomer({
+				'.custom_field_360016976631 .ember-checkbox': { event: 'click', scrollTo: true }
+			});
+		});
+	},
+
+	focusOnPanelCustomer: function(elements) {
+		var $panel = $('.ember-view.ticket-sidebar:visible');
+		$panel.find('[data-test-id="customer-context-tab-ticket"]').click();
+
+		setTimeout(() => {
+			$.each(elements, function (selector, obj) {
+				var $el = $panel.find(selector);
+				if ($el.length) {
+					if (obj.scrollTo) $panel.find('.sidebar_box_container').scrollTop($el.position().top);
+					$el[obj.event]();
+				}
+			});
+		}, 100);
+	},
+
+	setTabNavKeyCommands: function () {
+		hotkeys('ctrl+alt+9,command+alt+9,ctrl+alt+0,command+alt+0', function (event, handler) {
+			var type = handler.key == 'ctrl+alt+9' || handler.key == 'command+alt+9' ? 'prev' : 'next';
+			
+			console.log(ExtraCommands.logMessage.replace('%command%', 'zendesk-tab-' + type + ' ' + handler.key));
+
+			var $temp = ExtraCommands.$selectedTab[type]();
+			console.log($temp);
+			
+			if ($temp.length && !$temp.is('.add')) {
+				$temp.click();
+
+				ExtraCommands.clearAllTimings();
+				ExtraCommands.interval = setInterval(() => { $('*').blur(); }, 1);
+				ExtraCommands.timeout = setTimeout(() => { clearInterval(ExtraCommands.interval); }, 2000);
+			}
+		});
 	}
-});
+};
