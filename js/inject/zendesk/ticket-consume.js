68aea4b654bc31cca467222cfa4eeaf0fdc21dc3
joelthorner <joeltorner@gmail.com>
2019-08-08 20:15:29 +0200

    Zendesk - Automatic ticket consumption alert (by customer SLA)

--- js/inject/zendesk/ticket-consume.js
+++ js/inject/zendesk/ticket-consume.js
@@ -1,25 +1,73 @@
 TicketConsume = {
 
+	slas: {
+		silver: {
+			tiquets: 4,
+			levels: {
+				3: 'green',
+				4: 'yellow',
+				5: 'red'
+			}
+		},
+		gold: {
+			tiquets: 6,
+			levels: {
+				5: 'green',
+				6: 'yellow',
+				7: 'red'
+			}
+		},
+		platinum: {
+			tiquets: 10,
+			levels: {
+				9: 'green',
+				10: 'yellow',
+				11: 'red'
+			}
+		}
+	},
+
 	init: function () {
-		var ticketConsume_isTop = true;
+		var ticketConsume_isTop = true; // no remove
 
 		chrome.runtime.onMessage.addListener(function (details) {
 			log('Message from frame: ' + details);
-			console.log(JSON.parse(details));
 			var data = JSON.parse(details);
 			TicketConsume.evalCloseClosedMenu();
-			console.log(TicketConsume.getBadge(data));
+			var badge = TicketConsume.getBadge(data);
+			$('[data-tracking-id="tabs-nav-item-organizations"]').each(function() {
+				if ($(this).text().toLowerCase().trim() == data.client) {
+					var $nav = $(this).closest('nav.btn-group');
+					$nav.find('.TLmanaGer_ticketConsume_badge_cont').remove();
+					$nav.append(badge);
+				}
+			});
 		});
 	},
 
 	getBadge: function (data) {
+		var levels = TicketConsume.slas[data.sla].levels,
+				color = '';
+
+		data.nTiquets = 0;
+		
+		$.each(levels, function(index, this_color) {
+			console.log(data.nTiquets, index, this_color);
+			
+			if (data.nTiquets >= index) {
+				color = this_color;
+			}
+		});
+		
 		return `
-			<span class="TLmanaGer_ticketConsume_badge">
-			SLA ${data.sla.toUpperCase()} - ${data.nTiquets}/${6} TICKETS
-			</span>
-			<span class="TLmanaGer_ticketConsume_checkedAgo">
-				<span class="TLmanaGer_ticketConsume_checkedAgo_lbl">Last check</span>
-				<span class="TLmanaGer_ticketConsume_checkedAgo_text">${moment(data.check).fromNow()}</span>
+			<span class="TLmanaGer_ticketConsume_badge_cont">
+				<span class="TLmanaGer_ticketConsume_badge TLmanaGer_ticketConsume_badge_${color}">
+					SLA ${data.sla.toUpperCase()} - ${data.nTiquets}/${TicketConsume.slas[data.sla].tiquets} TICKETS
+				</span>
+				<span class="TLmanaGer_ticketConsume_checkedAgo">
+					<span class="TLmanaGer_ticketConsume_checkedAgo_lbl">Last check</span>
+					<span class="TLmanaGer_ticketConsume_checkedAgo_text">${moment(data.check).fromNow()}</span>
+				</span>
 			</span>
 		`;
 	},
