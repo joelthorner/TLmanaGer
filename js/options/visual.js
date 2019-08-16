5ad3b20121293579abe6b8868568c8e360d33462
joelthorner <joeltorner@gmail.com>
2019-08-17 01:30:04 +0200

    Changelog visual changes and add more complete info

--- js/options/visual.js
+++ js/options/visual.js
@@ -25,45 +25,57 @@ function setAvatars() {
 }
 
 function setChangelog() {
-	$.each(CHANGELOG, function(index, item) {
-		var listNew = [], listFix = [], listChange = [];
-		$.each(item.lines, function(index, line) {
-			if (line.indexOf('NEW') == 0) listNew.push(line.replace('NEW ', ''));
-			else if (line.indexOf('FIX') == 0) listFix.push(line.replace('FIX ', ''));
-			else if (line.indexOf('CHANGE') == 0) listChange.push(line.replace('CHANGE ', ''));
+	$.each(CHANGELOG, function (index, item) {
+		let lists = { new: [], fixed: [], improved: [], changed:[], removed: [] },
+			htmlLists = '', issueUrl = 'https://github.com/joelthorner/TLmanaGer/issues/';
+		
+		$.each(item.lines, function (index, line) {
+			let type = line.match(/^[A-Z]+/)[0];
+			let issue = line.match(/#\d{1,4}/);
+			let thisLine = line.replace(type + ' ', '');
+			if (issue) {
+				thisLine = thisLine.replace(issue[0], `<a href="${issueUrl}${issue[0].replace('#', '')}" target="_blank">${issue[0]}</a>`);
+			}
+			lists[type.toLowerCase()].push(thisLine);
 		});
+		$.each(lists, function (name, arr) {
+			if (arr.length) {
+				let items = '';
+				$.each(arr, function (index, arrItem) {
+					items += `<li>
+						<span class="mr-2 badge badge-${name}">${name.toUpperCase()}</span>
+						<span>${arrItem}</span>
+					</li>`;
+				});
+				htmlLists += `<ul class="items-list mb-3">${items}</ul>`;
+			}
+		});
+		appendChangelog(item, htmlLists);
+	});
+}
 
-		var NEW = '<span class="mr-2 badge badge-success">NEW</span>',
-			  FIX = '<span class="mr-2 badge badge-primary">FIX</span>',
-			  CHANGE = '<span class="mr-2 badge badge-change">CHANGE</span>';
-
-		$('#changelog .list-group').append(`
-			<li class="list-group-item list-group-item-changelog py-4">
-				<div class="row">
-					<a class="col-3 col-left media text-decoration-none" href="https://github.com/joelthorner/TLmanaGer/releases/tag/v${item.version}" target="_blank">
-						<svg class="icon mr-3"><use xlink:href="#icon-merge"></use></svg>
-						<div class="media-body mb-3">
-							<div class="title mb-0 text-light">${item.version}</div>
-							<!--<small class="text-secondary">${moment(new Date(item.date)).fromNow()}</small>-->
-							<small class="text-secondary">${moment(new Date(item.date)).format('MMMM Do YYYY')}</small>
-						</div>
-					</a>
-					
-					<div class="col-9">
-						<ul class="items-list mb-3 ${!listNew.length ? 'd-none' : ''}">
-							<li>${NEW}<span>${listNew.join('</span></li><li>' + NEW + '<span>')}</span></li>
-						</ul>
-						<ul class="items-list mb-3 ${!listChange.length ? 'd-none' : ''}">
-							<li>${CHANGE}<span>${listChange.join('</span></li><li>' + CHANGE + '<span>')}</span></li>
-						</ul>
-						<ul class="items-list mb-3 ${!listFix.length ? 'd-none' : ''}">
-							<li>${FIX}<span>${listFix.join('</span></li><li>' + FIX + '<span>')}</span></li>
-						</ul>
+function appendChangelog(item, htmlLists) {
+	$('#changelog .list-group').append(`
+		<li class="list-group-item list-group-item-changelog py-4">
+			<div class="row">
+				<a class="col-3 col-left media text-decoration-none" href="https://github.com/joelthorner/TLmanaGer/releases/tag/${item.version}" target="_blank">
+					<svg class="icon mr-3"><use xlink:href="#icon-merge"></use></svg>
+					<div class="media-body mb-3">
+						<div class="title mb-0 text-light">${item.version.replace('v', '')}</div>
+						<small class="text-secondary">${moment(new Date(item.date)).format('MMMM Do YYYY')}</small>
 					</div>
-				</div>
-			</li>
-		`);
-	}); 	
+				</a>
+				<div class="col-9">${htmlLists}</div>
+			</div>
+		</li>`);
+}
+
+function tabBodyClass($tab) {
+	$('body')
+		.removeClass(function (index, className) {
+			return (className.match(/body-tab-[a-zA-Z]+/g) || []).join(' ');
+		})
+		.addClass('body-tab-' + $tab.attr('href').replace('#', ''));
 }
 
 $(function() {
@@ -71,12 +83,14 @@ $(function() {
 	$('.version').text(chrome.runtime.getManifest().version);
 
 	if (location.hash.length) {
-		$('.navbar .navbar-nav [data-toggle="tab"][href="' + location.hash + '"]').click();
+		var $tab = $('.navbar .navbar-nav [data-toggle="tab"][href="' + location.hash + '"]').click();
+		tabBodyClass($tab);
 		execMasonry();
 	}
 
 	$('.navbar .navbar-nav [data-toggle="tab"]').click(function(event) {
 		location.hash = $(this).attr('href');
+		tabBodyClass($(this));
 		execMasonry();
 
 		$('.navbar .navbar-nav [data-toggle="tab"]').removeClass('show');
@@ -92,6 +106,5 @@ $(function() {
 	
 	setAvatars();
 	setChangelog();
-
 	execMasonry();
 });
