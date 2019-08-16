5c59b608a757666186f6cb9dd2ca0f0623bcbf57
Joelthorner <joeltorner@gmail.com>
2019-08-16 22:02:28 +0200

    Update optionsActions.js

--- js/optionsActions.js
+++ js/optionsActions.js
@@ -126,7 +126,7 @@ function restoreOptions() {
 			{ node: $('#opt-dev-dump-scroll-cfm')     , type: 'checkbox',    value: items.optDevDumpScrollCfm },
 			{ node: $('#opt-lc-big-controls')         , type: 'checkbox',    value: items.optLcBigControls },
 			{ node: $('#opt-lc-holidays')             , type: 'checkbox',    value: items.optLcHolidays },
-			{ node: $('#opt-lc-os-branches-btn')         , type: 'checkbox',    value: items.optLcOsBranchesBtn },
+			{ node: $('#opt-lc-os-branches-btn')      , type: 'checkbox',    value: items.optLcOsBranchesBtn },
 			{ node: $('#opt-zen-ticket-confirm')      , type: 'checkbox',    value: items.optZenTicketConfirm },
 			{ node: $('#opt-zen-prior-highs')         , type: 'checkbox',    value: items.optZenPriorHighs },
 			{ node: $('#opt-zen-prior-highs-incident'), type: 'checkbox',    value: items.optZenPriorHighsIncident },
