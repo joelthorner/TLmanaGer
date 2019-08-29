e51305a66111f8806943796a328cbfb3fd207373
joelthorner <joeltorner@gmail.com>
2019-08-29 23:15:11 +0200

    2.3.11

--- js/options/changelog.js
+++ js/options/changelog.js
@@ -1,6 +1,6 @@
 const CHANGELOG = [
 	{
-		version: 'v2.3.11', date: 'xx Aug 2019',
+		version: 'v2.3.11', date: '30 Aug 2019',
 		lines: [
 			'FIXED Zendesk ticket consumption alert - http iframe into https tab not work #163#issuecomment-525411887',
 			'CHANGED Changelog issue code adaptation #175'
