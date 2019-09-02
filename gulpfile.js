36138f43b8331bda9f5058904f7efba13a40dc03
joelthorner <joeltorner@gmail.com>
2019-09-02 19:34:43 +0200

    Add popper dependency

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const libsJs = [
 	'node_modules/hotkeys-js/dist/hotkeys.min.js',
 	'node_modules/js-cookie/src/js.cookie.js',
 	'node_modules/huebee/dist/huebee.pkgd.min.js',
+	'node_modules/popper.js/dist/popper.min.js',
 ]
 
 const scssBootstrap = [
