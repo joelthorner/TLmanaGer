c8ba176fb16eca36b0447a979ad8b9496de3b22f
joelthorner <joeltorner@gmail.com>
2019-09-04 23:42:58 +0200

    Fix #184

--- js/inject/zendesk/ticket-consume-source-tab.js
+++ js/inject/zendesk/ticket-consume-source-tab.js
@@ -5,26 +5,30 @@
  */
 
 function TicketConsumeTab_getDataTrs(trs) {
-	let data = ''
+	var data = '';
 	for (let index = 0; index < trs.length; index++) {
-		let organization = $(trs[index]).find('td:first-child').text().toLowerCase().trim(),
-			tickets = parseInt($(trs[index]).find('td:last-child').text()),
-			sla = $(trs[index]).closest('table').attr('id').replace('table', '').toLowerCase();
+		if ($(trs[index]).find('td').length) {
+			let organization = $(trs[index]).find('td:first-child').text().toLowerCase().trim(),
+				tickets = parseInt($(trs[index]).find('td:last-child').text()),
+				sla = $(trs[index]).closest('table').attr('id').replace('table', '').toLowerCase();
 
-		data += `
-			"${organization}": {
-				"tickets": ${tickets},
-				"sla": "${sla}"
-			}${index < trs.length - 1 ? ',' : ''}`;
+			data += `
+				"${organization}": {
+					"tickets": ${tickets},
+					"sla": "${sla}"
+				}${index < trs.length - 1 ? ',' : ''}`;
+		}
 	}
 	return data;
 }
 
-let data = '{';
+var data = '{';
 
 let si = setInterval(() => {
-	const doc = $(document).contents()[1];
-	let trs = doc.querySelectorAll('#tablePlatinium tbody tr, #tableGold tbody tr, #tableSilver tbody tr');
+	const doc = $(document)[0];
+	let trs = doc.querySelectorAll('#tablePlatinium tbody tr:not(.no-records-found), #tableGold tbody tr:not(.no-records-found), #tableSilver tbody tr:not(.no-records-found)');
+	console.log(trs);
+	
 	
 	if (trs.length) {
 		clearInterval(si);
