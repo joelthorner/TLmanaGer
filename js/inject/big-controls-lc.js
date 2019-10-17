1831a2f74e8dffdbddfd50cc49028b465ee231e5
joelthorner <joeltorner@gmail.com>
2019-10-18 00:48:12 +0200

    First commit

--- js/inject/big-controls-lc.js
+++
@@ -1,9 +0,0 @@
-chrome.storage.sync.get({ optLcBigControls: defaults.optLcBigControls }, function(result) {
-	
-	if (result.optLcBigControls && !$('#loginForm').length) {
-		
-		$('html').addClass('big-controls');
-
-	}
-
-});
