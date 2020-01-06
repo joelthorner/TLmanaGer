d6aec425b16f968aa4a1071410fa3f51e8cfce12
joelthorner <joeltorner@gmail.com>
2020-01-06 18:21:06 +0100

    Starts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'cf-root',
+  selector: 'tl-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'TLmanaGer3';
+  title = 'TLmanaGer';
 }
