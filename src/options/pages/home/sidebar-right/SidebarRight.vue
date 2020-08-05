f3c2a8826169de2c55c9070d223a51c2b1945c4b
joelthorner <joeltorner@gmail.com>
2020-08-06 01:40:48 +0200

    alpha 0.0.3

--- src/options/pages/home/sidebar-right/SidebarRight.vue
+++ src/options/pages/home/sidebar-right/SidebarRight.vue
@@ -1,8 +1,11 @@
 <template>
   <aside class="aside-right">
     <sidebar-right-resume>
-      <div class="title">VERSION</div>
-      <div class="content">{{ version }}</div>
+			<a :href="releaseLink" target="_blank" rel="noopener noreferrer">
+				<span class="title">VERSION</span>
+				<span class="content">{{ getVersion }}</span>
+				<span class="icon" v-html="caret"></span>
+			</a>
     </sidebar-right-resume>
   </aside>
 </template>
@@ -11,34 +14,44 @@
 import axios from "axios";
 
 import SidebarRightResume from "./SidebarRightResume";
+import { icons } from "./../../../../data.js";
 
 export default {
   name: "SidebarRight",
   data: () => {
     return {
-      version: "1.0.0",
+			version: "1.0.0",
+			caret: icons.caretRight
     };
   },
   components: {
     SidebarRightResume,
-  },
-  created() {
-    this.listReleases();
-  },
-  methods: {
-    listReleases() {
-      return axios
-        .get(`https://api.github.com/repos/joelthorner/TLmanaGer/releases`)
+	},
+	computed: {
+		getVersion() {
+			return chrome.runtime.getManifest().version;
+		},
+		releaseLink() {
+			return `https://github.com/joelthorner/TLmanaGer/releases/tag/v${this.getVersion}`;
+		},
+	}
+  // created() {
+  //   this.listReleases();
+  // },
+  // methods: {
+  //   listReleases() {
+  //     return axios
+  //       .get(`https://api.github.com/repos/joelthorner/TLmanaGer/releases`)
 
-        .then((response) => {
-          console.log(response.data[0]);
-          this.version = response.data[0].tag_name;
-        })
+  //       .then((response) => {
+  //         console.log(response.data[0]);
+  //         this.version = response.data[0].tag_name;
+  //       })
 
-        .catch((err) => {
-          console.log(err);
-        });
-    },
-  },
+  //       .catch((err) => {
+  //         console.log(err);
+  //       });
+  //   },
+  // },
 };
 </script>
