d7bffbec76b48bd6dca40a362b66bcea50cd405e
joelthorner <joeltorner@gmail.com>
2020-08-06 01:16:33 +0200

    alpha 0.0.2

--- src/data.js
+++ src/data.js
@@ -8,6 +8,106 @@ export const icons = {
 	github: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path></svg>',
 	bug: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M544 272h-64V150.627l35.313-35.313c6.249-6.248 6.249-16.379 0-22.627-6.248-6.248-16.379-6.248-22.627 0L457.373 128H417C417 57.26 359.751 0 289 0c-70.74 0-128 57.249-128 128h-42.373L75.314 84.687c-6.249-6.248-16.379-6.248-22.628 0-6.248 6.248-6.248 16.379 0 22.627L96 150.627V272H32c-8.836 0-16 7.163-16 16s7.164 16 16 16h64v24c0 36.634 11.256 70.686 30.484 98.889l-57.797 57.797c-6.249 6.248-6.249 16.379 0 22.627 6.249 6.249 16.379 6.248 22.627 0l55.616-55.616C178.851 483.971 223.128 504 272 504h32c48.872 0 93.149-20.029 125.071-52.302l55.616 55.616c6.249 6.249 16.379 6.248 22.627 0 6.249-6.248 6.249-16.379 0-22.627l-57.797-57.797C468.744 398.686 480 364.634 480 328v-24h64c8.837 0 16-7.163 16-16s-7.163-16-16-16zM289 32c53.019 0 96 42.981 96 96H193c0-53.019 42.981-96 96-96zm15 440V236c0-6.627-5.373-12-12-12h-8c-6.627 0-12 5.373-12 12v236c-79.402 0-144-64.599-144-144V160h320v168c0 79.401-64.599 144-144 144z"></path></svg>',
 }
-// export const settings = {
-// 	some: 'Settings'
-// }
+
+export const chromeData = {
+	zendesk: {
+		replyTicketConfirmPopup: {
+			actived: false
+		},
+		ticketPriorityHighlightColors: {
+			actived: false,
+			onlyIncidents: false,
+			colors: {
+				low: {
+					bg: '#fff',
+					text: '#222',
+				},
+				normal: {
+					bg: '#ffd',
+					text: '#222',
+				},
+				high: {
+					bg: '#fb7',
+					text: '#222',
+				},
+				urgent: {
+					bg: '#f77',
+					text: '#222',
+				},
+			}
+		},
+	},
+	logicommerce: {
+		beyondTheme: {
+			actived: false,
+			themeName: ''
+		},
+		background: {
+			actived: true,
+			image: 'https://images.unsplash.com/photo-1439694458393-78ecf14da7f9?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjM0NjEwfQ',
+			thumb: 'https://images.unsplash.com/photo-1439694458393-78ecf14da7f9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjM0NjEwfQ',
+			userName: 'Brandon Lam',
+			userLink: 'https://unsplash.com/@brandon_lam',
+			downloadLocation: 'https://api.unsplash.com/photos/Dd_7xDCuuUo/download?client_id=***REMOVED***',
+		},
+		developerBar: {
+			actived: true
+		},
+		sandboxLoginButtons: {
+			actived: true
+		},
+		pagesGridView: {
+			actived: true
+		}
+	},
+	fluidWebTools: {
+		autoForceView: {
+			actived: true
+		},
+		flushRedisIgdDomain: {
+			actived: true
+		},
+		dumpAutoScroll: {
+			actived: true
+		}
+	},
+	others: {
+		stealFontWesomeIcons: {
+			actived: true
+		},
+	},
+	profile: {
+		avatar: {
+			actived: true,
+			value: 'img/logo.svg',
+		},
+		shopTestingEmail: {
+			actived: true,
+			value: 'john.doe@example.com',
+		},
+		shopTestingUsername: {
+			actived: true,
+			value: 'John Doe',
+		},
+		shopTestingPassword: {
+			actived: true,
+			value: '123132',
+		}
+	},
+	metrics: {
+		timesBgChanged: 0,
+		zendeskActiveOptsCount: 0,
+		totalActiveOptsCount: 0,
+		openChangelogCount: 0,
+		syncGoogleAccount: false,
+		clickedIssuesAnchor: false,
+		clickedGithubAnchor: false,
+		clickedLegalsAnchor: false,
+		clickedActionCount: 0,
+		changedAvatarOpt: false,
+		extensionInstalled: false,
+		clickedResetData: false,
+		clickOnExtensionCount: 0,
+		openPopupCounter: 0,
+	}
+};
