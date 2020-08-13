72bca86b4347c64e72d6331f9ef6ea67fd2ff332
joelthorner <joeltorner@gmail.com>
2020-08-13 17:22:28 +0200

    alpha 0.0.18

--- src/options/pages/blog/sidebar-right/SidebarRightBlock.vue
+++ src/options/pages/blog/sidebar-right/SidebarRightBlock.vue
@@ -23,35 +23,14 @@
 
 <script>
 export default {
-  name: "SidebarRightContributors",
+  name: "SidebarRightBlock",
   data: () => {
     return {
-      contributors: [
-        {
-          name: "joelthorner",
-          route: "https://github.com/joelthorner",
-          img: "https://avatars2.githubusercontent.com/u/16495000?s=120&v=4",
-          role: "owner",
-        },
-        {
-          name: "pol-93",
-          route: "https://github.com/pol-93",
-          img: "https://avatars2.githubusercontent.com/u/16819509?s=120&v=4",
-          role: "contributor",
-        },
-        {
-          name: "Unoriginal02",
-          route: "https://github.com/Unoriginal02",
-          img: "https://avatars0.githubusercontent.com/u/11269504?s=120&v=4",
-          role: "beta tester",
-        },
-      ],
+      
     };
   },
   methods: {
-    linkTitle(name) {
-      return `View ${name}'s profile`;
-    },
+    
   },
 };
 </script>
