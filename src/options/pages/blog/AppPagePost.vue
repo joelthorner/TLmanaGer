a90bc6b4a6cb0b2f909c1d0a7d695135ecf4dc62
joelthorner <joeltorner@gmail.com>
2020-08-15 02:09:42 +0200

    alpha 0.0.23

--- src/options/pages/blog/AppPagePost.vue
+++ src/options/pages/blog/AppPagePost.vue
@@ -1,31 +1,43 @@
 <template>
-  <div>AppPagePost - {{ post.name }}</div>
+  <div class="page-content" v-if="finded">
+    <div id="post-content">
+      <main-title :title="post.name"></main-title>
+
+      <main-content containerClass="post-container"></main-content>
+    </div>
+  </div>
 </template>
 
 <script>
+import MainTitle from "./../../components/main/MainTitle.vue";
+import MainContent from "./../../components/main/MainContent.vue";
+
 import posts from "./../../../posts.js";
 
 export default {
   name: "AppPagePost",
-  props: {
-    id: Number,
+  components: {
+    MainTitle,
+    MainContent,
   },
   created() {
     this.getPostData();
   },
   data() {
     return {
+      id: parseInt(this.$route.params.id),
       post: {},
       finded: false,
     };
   },
   watch: {
-    id: function (newVal, oldVal) {
+    $route(to, from) {
+      this.id = parseInt(to.params.id);
       this.getPostData();
     },
   },
   methods: {
-    getPostData: function () {
+    getPostData() {
       this.post = {};
       this.finded = false;
 
---
+++ src/options/pages/options/tabs/TabOthers.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="tab">
+    TabOthers
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabOthers",
+};
+</script>
