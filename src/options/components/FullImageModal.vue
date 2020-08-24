87b4bc47092f2189f621b14c0af0711c12a045ee
joelthorner <joeltorner@gmail.com>
2020-08-24 04:04:56 +0200

    alpha 0.0.37

---
+++ src/options/components/FullImageModal.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-modal :id="thisModalId" size="lg" centered v-bind:title="title" hide-footer modal-class="full-image-modal">
+    <img :src="sourceUrl" :alt="title">
+  </b-modal>
+</template>
+
+<script>
+export default {
+  name: "FullImageModal",
+  props: {
+    thisModalId: String,
+    sourceUrl: String,
+    title: String,
+  },
+};
+</script>
