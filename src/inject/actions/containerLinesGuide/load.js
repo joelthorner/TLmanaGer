d830fd6148a4e707711f4db720af809d59bd3182
Joelthorner <joeltorner@gmail.com>
2020-09-30 12:36:33 +0200

    containerLinesGuide

---
+++ src/inject/actions/containerLinesGuide/load.js
@@ -0,0 +1,7 @@
+/**
+ * @file Initialize containerLinesGuide on load in all urls
+ *       The execution of this file has to go after ~/containerLinesGuide/index.js
+ * @author joelthorner
+ */
+
+containerLinesGuide.init(true);
