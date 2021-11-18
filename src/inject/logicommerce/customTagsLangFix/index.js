995edeedcdc3eaa4a1d9efd19c250ae230217cd4
Joelthorner <joeltorner@gmail.com>
2021-11-18 17:55:46 +0100

    Update

---
+++ src/inject/logicommerce/customTagsLangFix/index.js
@@ -0,0 +1,20 @@
+/**
+ * @file Initialize customTagsLangFix
+ * @author joelthorner
+ */
+'use strict';
+
+let refLangItem = document.querySelectorAll('#desktop .shortcut[onclick*="openOrders"]>div');
+if (refLangItem) {
+  let ordersWord = refLangItem.textContent.trim().toLowerCase();
+
+  if (ordersWord === 'pedidos') {
+    document.getElementsByTagName('html')[0].classList.add('customTagsLangFix-es');
+  }
+  else if (ordersWord === '进入订单管理') {
+    document.getElementsByTagName('html')[0].classList.add('customTagsLangFix-chi');
+  }
+  else {
+    document.getElementsByTagName('html')[0].classList.add('customTagsLangFix-en');
+  }
+}
